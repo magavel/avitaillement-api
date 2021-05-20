c7183fe25b2f636f0e7252667bb5639313ca85ce
feron <avel.feron@gmail.com>
2021-05-20 17:39:33 +0200

    ajout lmethode tableau des prix par stations

--- api/station/controllers/station.js
+++ api/station/controllers/station.js
@@ -1,8 +1,21 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+
+  async findGasoilsByStation(ctx) {
+    let response;
+    let values;
+    let dates;
+    const {id} = ctx.params;
+    const entitie = await strapi.services.station.findOne({id});
+    values = entitie.gasoils.map(obj => obj.price);
+    dates = entitie.gasoils.map(obj => obj.updatedAt);
+    response = {
+      values,
+      dates
+    }
+    return  response;
+  }
+};
